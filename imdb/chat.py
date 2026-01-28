d9d02611a1a175062f66dad1f5228a66203e9b01
Christophe Blefari <christophe.blefari@gmail.com>
2026-01-28 11:16:37 +0100

    Add the agentic loop

--- imdb/chat.py
+++ imdb/chat.py
@@ -9,18 +9,54 @@
 
 from openai import OpenAI
 import json
+from google.cloud import bigquery
 
 client = OpenAI()
 
-prompt = """
-You are an assistant that answer movies related questions.
-"""
 # TODO: add the schema of the IMDb database (BigQuery) in the prompt by reading it from BigQuery
 
 
+def get_prompt(schema):
+    base_prompt = """
+You are an assistant that answer movies related questions.
+"""
+    for table in schema:
+        base_prompt += f"\nTable: `{table['name']}`\n"
+        for column in table["schema"]:
+            base_prompt += f"- {column['name']} ({column['type']})\n"
+            if column["type"] == "RECORD" and column["fields"]:
+                for field in column["fields"]:
+                    base_prompt += f"\t- {field['name']} ({field['type']})\n"
+    return base_prompt
+
+
+def column_to_dict(column):
+    result = {"name": column.name, "type": column.field_type}
+    if column.field_type == "RECORD" and column.fields:
+        result["fields"] = [column_to_dict(field) for field in column.fields]
+    return result
+
+
+def get_schema():
+    client = bigquery.Client(project="ensai-2026")
+    dataset_id = "silver_christophe"
+    tables = ["movies", "dim_actors", "dim_directors"]
+
+    schema = []
+    client.get_dataset(dataset_id)
+    for table in tables:
+        table_schema = client.get_table(f"{dataset_id}.{table}").schema
+        table_schema = [column_to_dict(column) for column in table_schema]
+        schema.append({"name": dataset_id + "." + table, "schema": table_schema})
+    return schema
+
+
 def execute_sql(sql_query):
-    # TODO: write function to execute sql query on BigQuery
-    return []
+    print("Executing SQL query...")
+    client = bigquery.Client(project="ensai-2026")
+    query_job = client.query(sql_query)
+    results = query_job.result()
+    return results.to_dataframe().to_string()
 
 
 tools = [
@@ -43,18 +79,43 @@ tools = [
     },
 ]
 
-response = client.responses.create(
-    model="gpt-5-nano",
-    input=[
-        {"role": "system", "content": prompt},
-        {"role": "user", "content": "What is the last movie of Brad Pitt?"},
-    ],
-    tools=tools,  # type: ignore
-    reasoning=None,
-)
 
-for output in response.output:
-    if output.type == "function_call":
-        if output.name == "execute_sql":
-            result = execute_sql(json.loads(output.arguments)["sql_query"])
-            print(result)
+def run_agent(prompt):
+    print("Running agent...")
+    messages = [
+        {"role": "system", "content": prompt},
+        {
+            "role": "user",
+            "content": input("What do you want to know about the IMDb database?"),
+        },
+    ]
+    should_continue = True
+    while should_continue:
+        response = client.responses.create(
+            model="gpt-5.2",
+            input=messages,
+            tools=tools,  # type: ignore
+            reasoning=None,
+        )
+
+        for output in response.output:
+            if output.type == "function_call":
+                messages.append(output.model_dump())
+                if output.name == "execute_sql":
+                    result = execute_sql(json.loads(output.arguments)["sql_query"])
+                    messages.append(
+                        {
+                            "type": "function_call_output",
+                            "call_id": output.call_id,
+                            "output": json.dumps({"result": result}),
+                        }
+                    )
+            else:
+                should_continue = False
+
+        print(response.output_text)
+
+
+schema = get_schema()
+prompt = get_prompt(schema)
+run_agent(prompt)
